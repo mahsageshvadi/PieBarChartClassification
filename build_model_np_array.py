dc3b3a39fa7f1b638fb584d88f9c08a36a6c0d20
Mahsa Geshvadi <mahsa@Mahsas-MacBook-Air.local>
2022-12-13 12:11:05 -0500

    maraz

---
+++ build_model_np_array.py
@@ -0,0 +1,9 @@
+from image_generator_np_array import generate_data 
+
+
+images,labels = generate_data()
+
+
+
+
+from tensorflow.keras.models import Sequential
---
+++ image_generator_np_array.py
@@ -0,0 +1,156 @@
+image_width = 100
+image_height = 100
+number_of_channels = 3
+max_obj_num_for_bar = 6
+min_obj_num = 3
+max_thickness = 3
+number_of_images = 1000
+          
+number_of_piechart_images = number_of_images//2
+number_of_barchart_images = number_of_images//2
+max_obj_num_for_bar = 10
+max_obj_num_for_pie = 10
+
+import os
+
+
+if not os.path.exists('./Data'):
+  os.mkdir('Data')
+save_dir = './Data'
+
+
+
+import numpy as np
+import cv2
+import math
+import argparse
+
+
+def Normalize(arr):
+    return arr / np.sum(arr)
+    
+    
+
+def generate_barchart():
+    
+    
+    
+        image_log_for_statistics = {}
+        
+        colors = np.random.uniform(0.0, 0.9,size = (max_obj_num_for_bar,3))
+
+        image = np.ones(shape=(image_width, image_height, number_of_channels))
+        
+        number_of_bars = np.random.randint(2, max_obj_num_for_bar + 1)
+        thickness = np.random.randint(1, max_thickness)
+        padding = np.random.randint(0, 10)
+        ratio_for_padding = (image_width - 2* padding)/ image_width
+        height = np.random.randint(10, image_height * ratio_for_padding, size = number_of_bars)
+       
+        image_log_for_statistics['heigth'] = height
+        image_log_for_statistics['number_of_bars'] = number_of_bars
+
+        
+        barWidth = int( ((image_width-3*(number_of_bars+1)-3)//number_of_bars * (np.random.randint(50,100)/100.0))* ratio_for_padding )
+        barWidth = max(barWidth, 4)
+        spaceWidth = int((image_width-(barWidth)*number_of_bars)//(number_of_bars+1) * ratio_for_padding)
+
+        sx = (image_width - barWidth*number_of_bars - spaceWidth*(number_of_bars-1))//2
+        
+        
+        for j in range(number_of_bars):
+
+            sy = int(image_width * ratio_for_padding)
+            ex = sx + barWidth
+            ey = sy - height[j]
+            
+            if number_of_channels == 3:
+                cv2.rectangle(image,(sx,sy),(ex,ey),colors[j],-1)
+            else:
+                cv2.rectangle(image,(sx,sy),(ex,ey),0,thickness)
+
+            sx = ex + spaceWidth
+        noises = np.random.uniform(0, 0.05, (image_width, image_height,3))
+        image = image + noises
+        _min = 0.0
+        _max = image.max()
+        image -= _min
+        image /= (_max - _min)
+
+        
+
+     #   cv2.imwrite( save_dir+ '/Bar_image{}.PNG'.format(i), image* 255)
+
+        
+
+        return image
+    
+def generate_piechart():
+        
+
+            
+            number_of_pies = np.random.randint(2, max_obj_num_for_pie + 1)
+
+            max_w_h = max(image_width, image_height)/2
+            
+            r = np.random.randint(0.1* max_w_h,max_w_h -20)
+            thickness = np.random.randint(1,3)
+            
+            colors = np.random.uniform(0.0, 0.9,size = (max_obj_num_for_pie,3))
+            center = (int(image_width/2),int(image_height/2))
+            image = np.ones(shape=(image_width, image_height, 3))
+
+            angles = Normalize(np.random.randint(10,60,size=(number_of_pies)))
+
+            start_angle = 90 - np.random.randint(0,360*angles[0])/2.0
+            _cur_start_angle = start_angle
+            # cv2.circle(image,center,r,0,thickness)
+
+            for j in range(number_of_pies):
+                _cur_end_angle = _cur_start_angle + angles[j] * 360.0
+
+                cv2.ellipse(image, center, (r, r), 270, -_cur_start_angle, -_cur_end_angle, colors[j], -1)
+                _cur_start_angle = _cur_end_angle
+
+            noises = np.random.uniform(0, 0.05, (image_width, image_height,3))
+            image = image + noises
+
+            _min = 0.0
+            _max = image.max()
+            image -= _min
+            image /= (_max - _min)
+
+            
+          #  cv2.imwrite( save_dir + '/Pie_image{}.PNG'.format(i), image *255)
+                    
+            return image
+
+
+def generate_data():
+
+
+    #barchart_images = generate_barchart()
+   # piechart_images = generate_piechart()
+
+    images = np.ones((number_of_images,image_height, image_width, number_of_channels), dtype='float32')
+    labels = []
+    for i in range(number_of_barchart_images):
+      barchart_image = generate_barchart()
+      images[i] = barchart_image
+      label = np.ones(1)
+      labels.append(label)
+
+    for j in range(number_of_piechart_images):
+      piechart_image = generate_piechart()
+      images[number_of_barchart_images + j] = piechart_image
+      label = np.zeros(1)
+      labels.append(label)
+
+    
+    labels = np.array(labels)
+
+    return images, labels
+
+    
+
+
